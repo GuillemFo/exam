395d8f5df20709a3afd046e9ca2cc5bc6bc2fe90
Guillem Forns <132559287+GuillemFo@users.noreply.github.com>
2025-06-23 11:30:24 +0000

    try error

--- handmade_06.c
+++ handmade_06.c
@@ -1,9 +1,3 @@
-#include <errno.h>
-#include <string.h>
-#include <unistd.h>
-#include <netdb.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -18,55 +12,6 @@ void err_msg(char *s)
 	write(2,s, strlen(s));
 	exit(1);
 }
-
-
-int extract_message(char **buf, char **msg)
-{
-	char	*newbuf;
-	int	i;
-
-	*msg = 0;
-	if (*buf == 0)
-		return (0);
-	i = 0;
-	while ((*buf)[i])
-	{
-		if ((*buf)[i] == '\n')
-		{
-			newbuf = calloc(1, sizeof(*newbuf) * (strlen(*buf + i + 1) + 1));
-			if (newbuf == 0)
-				return (-1);
-			strcpy(newbuf, *buf + i + 1);
-			*msg = *buf;
-			(*msg)[i + 1] = 0;
-			*buf = newbuf;
-			return (1);
-		}
-		i++;
-	}
-	return (0);
-}
-
-char *str_join(char *buf, char *add)
-{
-	char	*newbuf;
-	int		len;
-
-	if (buf == 0)
-		len = 0;
-	else
-		len = strlen(buf);
-	newbuf = malloc(sizeof(*newbuf) * (len + strlen(add) + 1));
-	if (newbuf == 0)
-		return (0);
-	newbuf[0] = 0;
-	if (buf != 0)
-		strcat(newbuf, buf);
-	free(buf);
-	strcat(newbuf, add);
-	return (newbuf);
-}
-
 
 void notify_all(int author, char *str)
 {
@@ -118,22 +63,19 @@ int main(int ac, char **av) {
 	
 	int sockfd, connfd;
 	socklen_t len;
-	struct sockaddr_in servaddr, cli; 
 
+	//struct sockaddr_in servaddr, cli; 
+	
 	FD_ZERO(&afds);
 
-	sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+	//sockfd = socket(AF_INET, SOCK_STREAM, 0); 
 	if (sockfd < 0) { 
 		err_msg("Fatal error\n"); 
-	}  
-	bzero(&servaddr, sizeof(servaddr)); 
+	}
 
-	servaddr.sin_family = AF_INET; 
-	servaddr.sin_addr.s_addr = htonl(2130706433); //127.0.0.1
 	servaddr.sin_port = htons(atoi(av[1])); 
 
-
-	if ((bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != 0)
+	//if ((bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != 0)
 		err_msg("Fatal error\n"); 
 	if (listen(sockfd, 200) != 0)
 		err_msg("Fatal error\n"); 
@@ -176,5 +118,5 @@ int main(int ac, char **av) {
 			}
 		}
 	}
-	return (0);
+	return (0)
 }
